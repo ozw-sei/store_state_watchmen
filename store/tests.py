0f3216298635e20b30bd4e5e1a5916dc0da2d1a5
ozawaseijiro <seijiro4751@gmail.com>
2016-07-27 23:35:21 +0900

    ランキング圏外時のテストケース追加

--- store/tests.py
+++ store/tests.py
@@ -6,16 +6,26 @@ from store.app_store_ranking import AppStoreRanking
 
 class AppStoreRankingTest(unittest.TestCase):
     def setUp(self):
-        stub_file_url = "store/appstore.xml"
+        self.stub_file_url = "store/appstore.xml"
         self.app_id = "1094591345"  # pockemon go
         self.thing = AppStoreRanking(self.app_id, {})
-        self.thing.get_url = MagicMock(return_value=stub_file_url)
+        self.thing.get_url = MagicMock(return_value=self.stub_file_url)
 
     def test_app_store_ranking(self):
         self.thing.fetch_ranking()
         rank = self.thing.rank
 
         self.assertEqual(rank, 1)
+
+    def test_app_store_ranking_is_out_range(self):
+        thing = AppStoreRanking("000000000", {})
+        thing.get_url = MagicMock(return_value=self.stub_file_url)
+
+        thing.fetch_ranking()
+        rank = thing.rank
+
+        self.assertEqual(rank, 0)
+
 
     def test_app_store_ranking_taget_is_pockemongo(self):
         self.thing.fetch_ranking()
