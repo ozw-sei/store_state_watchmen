ef1f67aeedcf6fa7a00fabba9fa7d7b86455496c
tf-s.ozawa <s.ozawa@trifort.jp>
2016-07-28 11:42:42 +0900

    refactor test code

--- store/tests.py
+++ store/tests.py
@@ -4,32 +4,38 @@ from store.app_store_ranking import AppStoreRanking
 
 
 class AppStoreRankingTest(unittest.TestCase):
+
+
     def setUp(self):
-        self.stub_file_url = "store/appstore.xml"
-        self.app_id = "1094591345"  # pockemon go
-        self.thing = AppStoreRanking(self.app_id, {})
-        self.thing.get_url = MagicMock(return_value=self.stub_file_url)
+        self.pockemon_go_app_id = "1094591345"
+        self.rank_out_of_range_app_id = "000000000"
+
+    def get_standard_mock(self, app_id):
+        stub_file_url = "store/appstore.xml"
+        thing = AppStoreRanking(app_id, {})
+        thing.get_url = MagicMock(return_value=stub_file_url)
+        return thing
 
     def test_app_store_ranking(self):
-        self.thing.fetch_ranking()
-        rank = self.thing.rank
+        thing = self.get_standard_mock(self.pockemon_go_app_id)
+        thing.fetch_ranking()
+        rank = thing.rank
 
         self.assertEqual(rank, 1)
 
     def test_app_store_ranking_is_out_range(self):
-        thing = AppStoreRanking("000000000", {})
-        thing.get_url = MagicMock(return_value=self.stub_file_url)
+        thing = self.get_standard_mock(self.rank_out_of_range_app_id)
 
         thing.fetch_ranking()
         rank = thing.rank
-        target = self.thing.target
+        target = thing.target
 
         self.assertEqual(rank, 0)
         self.assertEqual(target.get("app_id"), None)
 
-
     def test_app_store_ranking_taget_is_pockemongo(self):
-        self.thing.fetch_ranking()
-        target = self.thing.target
+        thing = self.get_standard_mock(self.pockemon_go_app_id)
+        thing.fetch_ranking()
+        target = thing.target
 
-        self.assertEqual(target["app_id"], self.app_id)
+        self.assertEqual(target["app_id"], self.pockemon_go_app_id)
